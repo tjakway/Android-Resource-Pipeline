4e3075818cc21a506b115c0b24988c57f0886b1d
Thomas Jakway <tjakway@gmail.com>
2014-10-16 00:29:51 -0400

    wrote scripts to build the art processor and run it options are passed from the default release processing config file (art in & out dirs, overwrite option)
    helper source no longer calls the build quiz xml parser script
    added the art processor to the cleanup script

--- ArtProcessor/build_art_processor.sh
+++ ArtProcessor/build_art_processor.sh
@@ -1,3 +1,3 @@
+#!/usr/bin/env bash
 
-ant -buildfile build.xml create_run_jar
-sudo java -Xms1000m -Xmx2500m -Xss100m -jar art_processor.jar in/ out/ --overwrite=on
+sudo ant -buildfile build.xml create_run_jar
