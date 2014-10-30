7b77d9eabb5baacf30598b208c9c3fa169533222
Thomas Jakway <tjakway@gmail.com>
2014-10-30 11:25:24 -0400

    finished OptionsHandler

--- DPIStringsXMLGenerator/src/com/jakway/stringsgen/options/OptionsHandler.java
+++ DPIStringsXMLGenerator/src/com/jakway/stringsgen/options/OptionsHandler.java
@@ -1,14 +1,20 @@
 package com.jakway.stringsgen.options;
 
+import java.io.File;
+
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.UnrecognizedOptionException;
+
+import com.jakway.stringsgen.file.FileChecks;
 
 public class OptionsHandler
 {
 	private Options allOptions = new Options();
+	private static final String in="in", out="out", overwrite="overwrite", default_dpi="default-dpi";
 	
 	public OptionsHandler()
 	{
@@ -16,13 +22,13 @@ public class OptionsHandler
 		
 		Option options[] = {
 		//input folder option
-		new Option("in", "input", true, "The input folder--should be the 'assets' folder."),
+		new Option(in, "input", true, "The input folder--should be the 'assets' folder."),
 		//output folder option
-		new Option("out", "output", true, "The output folder.  values-xxxx (e.g. values-ldpi, values-mdpi...) subfolders will be created in this folder."),
+		new Option(out, "output", true, "The output folder.  values-xxxx (e.g. values-ldpi, values-mdpi...) subfolders will be created in this folder."),
 		//overwrite option (not required)
-		new Option("overwrite", true, "Optional --overwrite=on option to delete the output folder if it already exists before writing to it."),
+		new Option(overwrite, true, "Optional --overwrite=on option to delete the output folder if it already exists before writing to it."),
 		//default DPI option
-		new Option("default-dpi", true, "The DPI prefix to use for the unqualified values folder that Android accesses by default.  NOT the folder name itself.  Pass, for example, hdpi to copy the values-hdpi/strings.xml to values/strings.xml")
+		new Option(default_dpi, true, "The DPI prefix to use for the unqualified values folder that Android accesses by default.  NOT the folder name itself.  Pass, for example, hdpi to copy the values-hdpi/strings.xml to values/strings.xml")
 		};
 		
 		options[0].setRequired(true); //input folder
@@ -52,5 +58,52 @@ public class OptionsHandler
 		CommandLineParser parser = new GnuParser();
 		
 		parser.parse(allOptions, args);
+	}
+	
+	public File getInputFolder()
+	{
+		return new File(allOptions.getOption(in).getValue());
+	}
+	
+	public File getOutputFolder()
+	{
+		return new File(allOptions.getOption(out).getValue());
+	}
+	
+	/**
+	 * @throws UnrecognizedOptionException if the value passed to --overwrite is not "on" or "off"
+	 * @return true if the user passed --overwrite on, false if the user passed --overwrite off
+	 */
+	public boolean getIfOverwrite() throws UnrecognizedOptionException
+	{
+		String value = allOptions.getOption(overwrite).getValue();
+		
+		if(value.equals("on"))
+			return true;
+		else if(value.equals("off"))
+			return false;
+		else
+		{
+			throw new UnrecognizedOptionException("Did not recognize value passed with --"+overwrite+".  Acceptable values: on, off (--overwrite on, --overwrite off)", overwrite);
+		}
+	}
+	
+	/**
+	 * returns the DPI prefix the user selected to use for the default values folder
+	 * @return
+	 * @throws UnrecognizedOptionException if the user passed an invalid prefix
+	 */
+	public String getDefaultDPI() throws UnrecognizedOptionException
+	{
+		String value = allOptions.getOption(default_dpi).getValue();
+		
+		//check if the passed value is a valid prefix
+		for(String prefix : FileChecks.prefixes)
+		{
+			if(value.equals(prefix))
+				return value;
+		}
+		//user passed an invalid prefix
+		throw new UnrecognizedOptionException("Did not recognize value passed with --"+default_dpi+".  Acceptable values include ldpi, mdpi, hdpi, etc.", default_dpi);
 	}
 }
